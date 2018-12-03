43dd391c04df34403be60fe366b1f8587f2f9a43
Mr DaHoM <44880953+mrdahom4@users.noreply.github.com>
2018-12-03 13:17:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -90,9 +90,6 @@ return;
         message.guild.members.forEach(m => {
    if(!message.member.hasPermission('ADMINISTRATOR')) return;
             var bc = new Discord.RichEmbed()
-            .addField('» السيرفر :', `${message.guild.name}`)
-            .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
-            .addField(' » الرسالة : ', args)
             .setColor('#ff0000')
             // m.send(`[${m}]`);
             m.send(`${m}`,{embed: bc});
